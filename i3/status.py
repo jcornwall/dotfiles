be901a7e5c0a39094a441347ea142554cd272c3c
Jay Cornwall <jay@jcornwall.me>
2016-11-27 19:05:42 -0600

    i3: Configuration updates
    - Bar: add maildir indicator
    - Bar: add custom VPN management interface
    - Bar: remove systray

--- i3/status.py
+++ i3/status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from datetime import datetime
-import i3pystatus, os, subprocess
+import dbus.mainloop.glib, gi.repository.GObject
+import i3pystatus, i3pystatus.mail.maildir, os, subprocess, threading
 
 class RedshiftTimed(i3pystatus.IntervalModule):
   settings = (
@@ -107,6 +108,83 @@ class RedshiftTimed(i3pystatus.IntervalModule):
 
     return time_mins
 
+class VPNManager(i3pystatus.Module):
+  settings = (
+    "color_disabled",
+    "format",
+    "vpn_name",
+  )
+
+  color_disabled = "#48413a"
+  format = "  <span color='#b7ba53'></span>   {vpn_name} "
+  vpn_name = None
+
+  enabled = False
+
+  def init(self):
+    thread = threading.Thread(target = self.monitor_nm)
+    thread.start()
+
+  def monitor_nm(self):
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default = True)
+    import NetworkManager
+
+    def nm_props_changed(*args, **kwargs):
+      self.enabled = False
+      self.vpn_name = "VPN"
+
+      for conn in NetworkManager.NetworkManager.ActiveConnections:
+        if conn.Vpn:
+          self.enabled = True
+          self.vpn_name = conn.Connection.GetSettings()["connection"]["id"]
+
+      self.update_output()
+
+    nm_props_changed()
+    NetworkManager.NetworkManager.connect_to_signal('PropertiesChanged', nm_props_changed)
+
+    def activate_vpn():
+      if self.enabled:
+        return
+
+      for conn in NetworkManager.Settings.ListConnections():
+        if conn.GetSettings()["connection"]["type"] == "vpn":
+          for dev in NetworkManager.NetworkManager.GetDevices():
+            if dev.State == NetworkManager.NM_DEVICE_STATE_ACTIVATED and dev.Managed:
+              break
+
+          NetworkManager.NetworkManager.ActivateConnection(conn, dev, "/")
+          break
+
+    def deactivate_vpn():
+      if not self.enabled:
+        return
+
+      for conn in NetworkManager.NetworkManager.ActiveConnections:
+        if conn.Vpn:
+          NetworkManager.NetworkManager.DeactivateConnection(conn)
+          break
+
+    self.on_leftclick = activate_vpn
+    self.on_rightclick = deactivate_vpn
+
+    loop = gi.repository.GObject.MainLoop()
+    loop.run()
+
+  def update_output(self):
+    fdict = {
+      "vpn_name" : self.vpn_name,
+    }
+
+    self.output = {
+      "full_text" : i3pystatus.formatp(self.format, **fdict),
+    }
+
+    if not self.enabled:
+      self.output["color"] = self.color_disabled
+
+    self.send_output()
+
 status = i3pystatus.Status()
 
 status.register(
@@ -123,7 +201,7 @@ status.register(
 
 status.register(
   "clock",
-  format = "  <span color='#b7ba53'></span>   %a %-d %b ",
+  format = "  <span color='#b7ba53'></span>   %a %-d %b ",
   hints = {
     "markup" : "pango",
     "background" : "#231e18",
@@ -137,6 +215,33 @@ status.register(
   "pulseaudio",
   format = "  <span color='#b7ba53'></span>   {volume}% ",
   color_muted = "#48413a",
+  hints = {
+    "markup" : "pango",
+    "background" : "#231e18",
+    "border" : "#231e18",
+    "separator_block_width" : 10,
+  }
+)
+
+status.register(
+  "mail",
+  format = "  <span color='#b7ba53'></span>   {unread} ",
+  backends = [
+    i3pystatus.mail.maildir.MaildirMail(directory = os.path.expanduser("~/mail/INBOX"))
+  ],
+  hide_if_null = False,
+  color = "#c79668",
+  color_unread = "#c79668",
+  hints = {
+    "markup" : "pango",
+    "background" : "#231e18",
+    "border" : "#231e18",
+    "separator_block_width" : 10,
+  }
+)
+
+status.register(
+  VPNManager,
   hints = {
     "markup" : "pango",
     "background" : "#231e18",
